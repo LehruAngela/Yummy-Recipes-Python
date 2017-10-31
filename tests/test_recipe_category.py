5e47d38ff79cfb65c704618e435e81e2468be365
LehruAngela <angelalehru@gmail.com>
2017-10-31 23:50:52 +0300

    Amended test_recipe_category file

--- tests/test_recipe_category.py
+++ tests/test_recipe_category.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.models import RecipeCategory
+from app.models.recipeCategory import RecipeCategory
 
 class TestRecipeCategory(unittest.TestCase):
     def setUp(self):
@@ -8,16 +8,16 @@ class TestRecipeCategory(unittest.TestCase):
 
     def test_add_recipe_to_recipe_category(self):
         self.recipe_category.add_recipe_to_recipe_category('Chicken Stew')
-        self.assertEqual(self.recipe_category.view_recipe_categories(), ['Chicken Stew'])
+        self.assertEqual(self.recipe_category.view_recipes(), ['Chicken Stew'])
 
     def test_delete_recipe_from_recipe_category(self):
         self.recipe_category.add_recipe_to_recipe_category('Chicken Stew')
         self.recipe_category.add_recipe_to_recipe_category('Beef Stew')
         self.recipe_category.delete_recipe_from_recipe_category('Chicken Stew')
-        self.assertEqual(self.recipe_category.view_recipe_categories(), ['Beef Stew'])
+        self.assertEqual(self.recipe_category.view_recipes(), ['Beef Stew'])
 
     def test_view_recipe_categories(self):
         self.recipe_category.add_recipe_to_recipe_category('Chicken Stew')
         self.recipe_category.add_recipe_to_recipe_category('Beef Stew')
         self.recipe_category.add_recipe_to_recipe_category('Ham Stew')
-        self.assertEqual(self.recipe_category.view_shopping_list(), ['Chicken Stew', 'Beef Stew', 'Ham Stew'])
+        self.assertEqual(self.recipe_category.view_recipes(), ['Chicken Stew', 'Beef Stew', 'Ham Stew'])
