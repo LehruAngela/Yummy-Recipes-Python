3d3493e5a4b5fed176975003b2aa7d8cd7d554a7
LehruAngela <angelalehru@gmail.com>
2017-10-31 23:47:20 +0300

    Amended comments in the user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 class User(object):
-    """Class for user attributes and methods"""
+    """class for user attributes and methods"""
     def __init__(self, username, email, password, recipe_categories=None):
         self.username = username
         self.email = email
@@ -7,13 +7,13 @@ class User(object):
         self.recipe_categories = recipe_categories or []
 
     def create_recipe_category(self, recipe_category):
-        """Method to create shopping list"""
+        """Method to add a recipe category"""
         self.recipe_categories.append(recipe_category)
 
     def delete_recipe_category(self, recipe_category):
-        """Method to delete shopping list"""
+        """Method to delete a recipe category"""
         self.recipe_categories.remove(recipe_category)
 
     def view_recipe_categories(self):
-        """Method to view shopping lists"""
+        """Method to view recipe categories created"""
         return self.recipe_categories
