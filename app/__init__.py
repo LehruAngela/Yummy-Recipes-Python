b2299a35bbfdc6a32617a1e16b6789d85528285f
LehruAngela <angelalehru@gmail.com>
2017-11-02 16:16:19 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+# App configs
 app.config['SESSION_TYPE'] = "filesystem"
 
 # Load the views
 from app import views
 
-
+# load the config file
+app.config.from_object('config')
 
 
 
