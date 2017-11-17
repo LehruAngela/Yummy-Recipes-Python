d11650164897f29828080c7f78cffd34a80735f0
LehruAngela <angelalehru@gmail.com>
2017-11-17 18:31:55 +0300

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,17 +6,17 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.user = User('Angela', 'angelalehru@gmail.com', '1234')
 
-    def test_create_shopping_list(self):
+    def test_create_recipe_category(self):
         self.user.create_recipe_category('bags')
         self.assertEqual(self.user.view_recipe_categories(), ['bags'])
 
-    def test_delete_shopping_list(self):
+    def test_delete_recipe_category(self):
         self.user.create_recipe_category('cakes')
         self.user.create_recipe_category('stews')
         self.user.delete_recipe_category('cakes')
         self.assertEqual(self.user.view_recipe_categories(), ['stews'] )
 
-    def test_view_shopping_lists(self):
+    def test_view_recipe_categories(self):
         self.user.create_recipe_category('cakes')
         self.user.create_recipe_category('stews')
         self.user.create_recipe_category('salads')
